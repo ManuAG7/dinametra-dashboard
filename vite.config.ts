481623eac2077a99025de70008cd6d4b1f6fad30
alami <alamillagmanuel7@gmail.com>
2026-03-05 10:18:25 -0600

    correcciones vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
+  server: {
+    proxy: {
+      "/cg": {
+        target: "https://api.coingecko.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/cg/, ""),
+      },
+    
+  },
+  },
+
   test: {
     environment: "jsdom",
     globals: true,
