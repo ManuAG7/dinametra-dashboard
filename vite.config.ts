7bd9e8ca73dceafa643f4a1acadee288306bbe19
alami <alamillagmanuel7@gmail.com>
2026-03-05 09:16:06 -0600

    fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,6 @@ export default defineConfig({
 
   server: {
     proxy: {
-      "/cg": {
-        target: "https://api.coingecko.com",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/cg/, ""),
-      },
-    
   },
   },
 
